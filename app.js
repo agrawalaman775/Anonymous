55efb4cc93fde2a78d8d4ee91c76ec99c040a359
Aman <agrawalaman977@gmail.com>
2023-07-09 02:58:01 +0530

    Google api server deploy

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://anonymous-2mlp.onrender.com/auth/google/secrets",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
     
   },
